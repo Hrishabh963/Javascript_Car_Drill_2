a512341056cde1f63680df04c52399b11bb2305b
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-08-28 16:13:56 +0530

    Third Problem completed
    Use map to map the required values to a new object and sort it

---
+++ problem3.js
@@ -0,0 +1,4 @@
+module.exports = function sortCarModelsAlphabetically(inventory) {
+    const carModels = inventory.map(car => car.car_model);
+    return carModels.sort();
+}
---
+++ test/testProblem3.js
@@ -0,0 +1,4 @@
+const inventory = require('../inventory');
+const problem3 = require('../problem3');
+const sortedCarModels = problem3(inventory);
+console.log(sortedCarModels);
